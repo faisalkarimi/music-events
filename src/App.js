60dace6d776c8ba4d0a5e550238247df8ba282d3
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-11-18 20:38:56 -0800

    Added Hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <BrowserRouter>
       <Header user="faisal" />
+      <Hero />
     </BrowserRouter>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="showcase">
+      <h1>Welcome To The Party!</h1>
+      <h2>Find the hottest DJ events</h2>
+    </div>
+  );
+};
+
+export default Hero;
